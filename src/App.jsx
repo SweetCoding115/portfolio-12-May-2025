41943ff3007cce29a1583f351db172bca46b9bd0
SweetCoding115 <jessie.pangilinan11111988@gmail.com>
2025-05-13 10:50:09 +0100

    replace spaces with tabs

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import AtroposCard from './components/atropos'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <AtroposCard />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<AtroposCard />
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+		</>
+	)
 }
 
 export default App
